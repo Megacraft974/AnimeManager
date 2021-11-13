3ee0ca54dd31b23298826497837e4e3310be22cb
Megacraft974 <william@michaud.re>
2021-11-13 09:47:32 +0400

    'AddingBaseCode'

---
+++ search_engines/tokyotosho.py
@@ -0,0 +1,72 @@
+""" Torrent web parser for nyaa.si """
+import urllib.parse
+import requests
+import io
+import re
+try:
+    from bs4 import BeautifulSoup
+except ModuleNotFoundError:
+    import os
+    os.system("pip install bs4")
+    from bs4 import BeautifulSoup
+
+
+class Parser():
+    def search(self, terms, limit=50):
+        terms = terms.strip()
+        searchterms = urllib.parse.quote_plus(terms)
+
+        soup = None
+        url = "https://www.tokyotosho.info/search.php?terms={}&type=1&searchName=true&searchComment=true".format(
+            searchterms)
+        try:
+            # print(url)
+            r = requests.get(url, timeout=10)
+        except requests.exceptions.ConnectionError:
+            print("Tokyotosho - No internet connection!")
+        except requests.exceptions.ReadTimeout:
+            print("Tokyotosho - Timed out!")
+        else:
+            # TODO - Error handling
+            soup = BeautifulSoup(r.content, "html.parser")
+            pattern = re.compile(r'\| Size: (\S*?) \|')
+            table = soup.find("table", class_="listing")
+            body = table.find_all("tr")[1:]
+            for rowA, rowB in self.table_iter(body):
+                try:
+                    title_column = rowA.find("td", class_="desc-top")
+                    filename = title_column.find_all("a")[-1].text
+                    torrent_url = title_column.find_all("a")[-1]['href']
+
+                    desc = rowB.find("td", class_="desc-bot").text
+                    result = pattern.findall(desc)
+                    file_size = result[0] if len(result) >= 1 else ""
+
+                    stats = rowB.find("td", class_="stats")
+                    seeds, leechs = map(
+                        lambda e: e.text, stats.find_all("span")[:2])
+
+                    out = {'filename': filename, 'torrent_url': torrent_url,
+                           'seeds': seeds, 'leechs': leechs, 'file_size': file_size}
+                    yield out
+                except Exception as e:
+                    print("Tokyotosho - error:", e)
+
+    def table_iter(self, table):
+        a, b = None, None
+        for e in table:
+            if a is None:
+                a = e
+            elif b is None:
+                b = e
+                yield a, b
+                a, b = None, None
+
+
+if __name__ == "__main__":
+    p = Parser()
+
+    r = p.search("meikyu")
+    for m in r:
+        # pass
+        print(m)
---
+++ test.py
@@ -0,0 +1,73 @@
+# import re,requests,urllib,socket
+
+# url = "magnet:?xt=urn:btih:RRN4MNLGBHMAVYU372YXQ46NU3WVZ5WR&dn=%5BSubsPlease%5D%20Mushoku%20Tensei%20-%2015%20%28720p%29%20%5B14A68BE1%5D.mkv&xl=686477749&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker3.itzmx.com%3A6961%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fretracker.lanta-net.ru%3A2710%2Fannounce&tr=http%3A%2F%2Fopen.acgnxtracker.com%3A80%2Fannounce&tr=wss%3A%2F%2Ftracker.openwebtorrent.com"
+# pattern = re.compile(r"^magnet:?")
+
+# print(str("0x41727101980"))
+
+# def getTracker(protocol,url,port,info_hash,size):
+# 	params = {
+# 	    'info_hash': info_hash,
+# 	    'peer_id': "weshweshcava12345678",
+# 	    'port': port,
+# 	    'uploaded': '0',
+# 	    'downloaded': '0',
+# 	    'left': str(size),
+# 	    'compact': '1',
+# 	    'no_peer_id': '0',
+# 	    'event': 'started'
+# 	}
+# 	if protocol == "http":
+# 		try:
+# 			page = requests.get(url, params=params)
+# 		except ConnectionResetError as e:
+# 			print(e)
+# 		except requests.exceptions.ConnectionError as e:
+# 			print(2,e)
+# 		except requests.exceptions.InvalidSchema as e:
+# 			print(3,e)
+# 		else:
+# 			print("---",page.text)
+# 	elif protocol == "udp":
+# 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+# 		sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+# 	else:
+# 		print(protocol,"not supported")
+
+# if pattern.match(url):
+# 	print("Magnet!")
+
+# 	parameters = re.compile(r"(\w{2})=([^&]+)")
+# 	data = list((a,urllib.parse.unquote(b)) for a,b in parameters.findall(url))
+# 	for i,d in enumerate(data):
+# 		k,v = d
+# 		if k == "tr":
+# 			try:
+# 				v = urllib.parse.urlparse(v)
+# 			except Exception as e:
+# 				print(e)
+# 		else:
+# 			print(k,v)
+# 		data[i] = (k,v)
+
+# 	print("___________")
+# 	info_hash = [d[1] for d in data if d[0] == "xt"][0].split(":")[-1]
+# 	size = [d[1] for d in data if d[0] == "xl"][0]
+# 	print(info_hash,size)
+# 	for k,v in (d for d in data if d[0] == "tr"):
+# 		url = urllib.parse.urlunparse(v)
+# 		print(url)
+# 		getTracker(v.scheme,url,v.port,info_hash,size)
+
+from dbManager import db
+from animeAPI import AnimeAPI
+import os
+
+appdata = os.path.join(os.getenv('APPDATA'), "AnimeManager")
+dbPath = os.path.join(appdata, "animeData.db")
+database = db(dbPath)
+api = AnimeAPI('all', dbPath)
+
+sql = "SELECT id,title FROM anime WHERE broadcast is not null"
+for a in database.sql(sql):
+    data = api.anime(a[0])
